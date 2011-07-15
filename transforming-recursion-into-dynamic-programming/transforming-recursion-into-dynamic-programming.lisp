8ae4ec399defddb49df3fa5913124f701c14c33f
Kragen Javier Sitaker <kragen@canonical.org>
2011-07-15 15:51:56 -0300

    Added copyright notice to dynamic programming hack.

--- transforming-recursion-into-dynamic-programming/transforming-recursion-into-dynamic-programming.lisp
+++ transforming-recursion-into-dynamic-programming/transforming-recursion-into-dynamic-programming.lisp
@@ -1,5 +1,5 @@
 ;;; Transforming recursion into dynamic programming.
-;;; By Darius Bacon.  No license provided.
+;;; © 1994-2003 by Darius Bacon.  No license provided.
 
 (defmacro defun-recurrence (name params inits &body body)
   "Define a recursive function that's evaluated by dynamic programming,
